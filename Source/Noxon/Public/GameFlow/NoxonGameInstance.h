33eba4ae9073e3a89949debbcf6922471f1ae599
VaVamVa <114375712+VaVamVa@users.noreply.github.com>
2025-09-03 17:51:13 +0900

    ReName Project and Git Directory Structure

---
+++ Source/Noxon/Public/GameFlow/NoxonGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NoxonGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOXON_API UNoxonGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
