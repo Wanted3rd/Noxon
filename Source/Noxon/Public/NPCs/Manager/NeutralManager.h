90352bd35527e8bb0bf264f552672f9f4ff3d45b
VaVamVa <114375712+VaVamVa@users.noreply.github.com>
2025-09-04 08:44:44 +0900

    Create NPC Managers

---
+++ Source/Noxon/Public/NPCs/Manager/NeutralManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseNPCManager.h"
+#include "NeutralManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOXON_API UNeutralManager : public UBaseNPCManager
+{
+	GENERATED_BODY()
+};
