2003a9044b271250294196b5a0bb1634b4d16066
VaVamVa <114375712+VaVamVa@users.noreply.github.com>
2025-09-03 18:14:44 +0900

    Create base NPC for Example

---
+++ Source/Noxon/Private/NPCs/BaseNonPlayableCharacter.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPCs/BaseNonPlayableCharacter.h"
+
+
+// Sets default values
+ABaseNonPlayableCharacter::ABaseNonPlayableCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABaseNonPlayableCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseNonPlayableCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABaseNonPlayableCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
