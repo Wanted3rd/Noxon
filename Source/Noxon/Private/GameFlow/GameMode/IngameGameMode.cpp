b987b47da7628aa5b6424b12ac153f318093fabc
VaVamVa <114375712+VaVamVa@users.noreply.github.com>
2025-09-04 19:39:10 +0900

    Make Utilitis and Solve Assets Mapping Error

--- Source/Noxon/Private/GameFlow/GameMode/IngameGameMode.cpp
+++ Source/Noxon/Private/GameFlow/GameMode/IngameGameMode.cpp
@@ -2,3 +2,22 @@
 
 
 #include "GameFlow/GameMode/IngameGameMode.h"
+
+#include "NPCs/Manager/EnemyManager.h"
+#include "NPCs/Manager/NeutralManager.h"
+#include "Utility/FindHelper.h"
+
+AIngameGameMode::AIngameGameMode()
+{
+	//DefaultPawnClass = ;
+	
+	EnemyManager = CreateDefaultSubobject<UEnemyManager>("EnemyManager");
+	NeutralManager = CreateDefaultSubobject<UNeutralManager>("NeutralManager");
+	
+}
+
+void AIngameGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
