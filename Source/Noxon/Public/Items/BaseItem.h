71ae15832720d89379031c0c37ca42ef07aad7b1
VaVamVa <114375712+VaVamVa@users.noreply.github.com>
2025-09-08 18:36:11 +0900

    Create Base Item Classes

--- Source/Noxon/Public/Items/BaseItem.h
+++ Source/Noxon/Public/Items/BaseItem.h
@@ -4,18 +4,11 @@
 #include "GameFramework/Actor.h"
 #include "BaseItem.generated.h"
 
-UCLASS()
-class NOXON_API ABaseItem : public AActor
+USTRUCT(BlueprintType)
+struct FItemProperty
 {
 	GENERATED_BODY()
 
-public:
-	ABaseItem();
-	
-protected:
-	virtual void BeginPlay() override;
-
-protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UTexture2D> icon = nullptr;
 	
@@ -25,8 +18,44 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	FString description = "";
 
+	// 스택 가능한 양
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool bQuantity = false;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	int quantity = 0;
+	// 내구도
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	bool stackable = false;
+	bool bDurability = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float durability = 0.f;
+	// 총알
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool bMagazine = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int magazine = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float deal = 0.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float heal = 0.f;
+};
+
+UCLASS()
+class NOXON_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABaseItem();
+
+	UFUNCTION(BlueprintPure)
+	const FItemProperty& GetItemProperty() const {return property;}
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FItemProperty property = FItemProperty();
+	
 };
